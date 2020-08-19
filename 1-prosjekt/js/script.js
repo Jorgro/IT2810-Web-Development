5e2e974965696566ddca72f19f19ef4bc12bca0c
Jørgen Rosager <joergen.rosager@gmail.com>
2020-08-19 11:41:59 +0200

    add button for documentation

--- 1-prosjekt/js/script.js
+++ 1-prosjekt/js/script.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
     $("#demo").html("Fuck this");
     $("#rect1").bind("mouseover", function(event) {
-        $("#rect1").css({ fill: "red" });
+        $("#rect1").css({
+            fill: "red"
+        });
     });
     $("#rect1").bind("mouseout", function(event) {
-        $("#rect1").css({ fill: "black" });
+        $("#rect1").css({
+            fill: "black"
+        });
+    });
+    $("#documentationButton").click(function() {
+        console.log("Clicked");
+        if ($("#documentation").css("display") == "none") {
+            console.log("None");
+
+            $("#documentation").css({
+                display: "block"
+            });
+        } else {
+            console.log("Display");
+
+            $("#documentation").css({
+                display: "none"
+            });
+        }
     });
 });
