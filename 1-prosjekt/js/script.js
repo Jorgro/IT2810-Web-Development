00738327917479c2bb6e87c2ad04efc3427590f3
Jørgen Rosager <joergen.rosager@gmail.com>
2020-08-19 11:24:10 +0200

    add flexbox

--- 1-prosjekt/js/script.js
+++ 1-prosjekt/js/script.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
     $("#rect1").bind("mouseover", function(event) {
         $("#rect1").css({ fill: "red" });
     });
+    $("#rect1").bind("mouseout", function(event) {
+        $("#rect1").css({ fill: "black" });
+    });
 });
