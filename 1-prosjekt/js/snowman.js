5882c2710b8b114c35a33d8c12a7167b68a15f81
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-08-20 13:53:25 +0200

    added click functionality to snowman canvas

--- 1-prosjekt/js/snowman.js
+++ 1-prosjekt/js/snowman.js
@@ -9,19 +9,6 @@ ctx.moveTo(115,45);
 ctx.arc(100,85,40,(24/15)*Math.PI,(21/15)*Math.PI);
 ctx.stroke();
 
-//head
-ctx.moveTo(120,30);
-ctx.arc(100,30,20,0,2*Math.PI);
-ctx.stroke();
-
-ctx.moveTo(97,25);
-ctx.arc(95,25,2,0,2*Math.PI);
-ctx.stroke();
-
-ctx.moveTo(110,25);
-ctx.arc(108,25,2,0,2*Math.PI);
-ctx.stroke();
-
 //left arm
 ctx.moveTo(65,65);
 ctx.lineTo(25,45);
@@ -40,6 +27,24 @@ ctx.lineTo(155,55);;
 ctx.lineTo(170,35);
 ctx.stroke();
 
+//head
+const head = {
+    x: 100,
+    y: 30,
+    radius: 20
+}
+ctx.moveTo(120,30);
+ctx.arc(head.x, head.y, head.radius,0,2*Math.PI);
+ctx.stroke();
+
+ctx.moveTo(97,25);
+ctx.arc(95,25,2,0,2*Math.PI);
+ctx.stroke();
+
+ctx.moveTo(110,25);
+ctx.arc(108,25,2,0,2*Math.PI);
+ctx.stroke();
+
 //carrot
 ctx.lineWidth = 1;
 ctx.beginPath();
@@ -49,3 +54,13 @@ ctx.lineTo(112,37);
 ctx.closePath();
 ctx.fillStyle = "orange";
 ctx.fill();
+
+//clickable head function
+function intersects(x, y, circle){
+    return ((x - circle.x)**2 + (y - circle.y)**2) < circle.radius**2;
+}
+canvas.addEventListener('click', (e) => {
+    if(intersects(e.offsetX, e.offsetY, head)){
+        alert("Clicked!");
+    }
+});
